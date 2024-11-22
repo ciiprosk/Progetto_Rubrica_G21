57b0bf548f1c2e89df320c08fdd7b09867ee3fd2
rosap <rosap@172.19.166.74>
2024-11-22 09:46:16 +0100

    primo commit rosa

---
+++ src/main/java/rubrica/porgetto_rubrica_g21/Porgetto_Rubrica_G21.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package rubrica.porgetto_rubrica_g21;
+
+/**
+ *
+ * @author rosap
+ */
+public class Porgetto_Rubrica_G21 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
