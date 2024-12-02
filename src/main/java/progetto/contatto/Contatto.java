5839f08d7f7602a0098aecc8317ae99f8a956a65
anton <anton@172.19.206.117>
2024-12-02 16:12:33 +0100

    scheletro contatto

--- src/main/java/progetto/contatto/Contatto.java
+++ src/main/java/progetto/contatto/Contatto.java
@@ -11,6 +11,65 @@ package progetto.contatto;
 public class Contatto {
     private String nome;
     private String cognome;
-    private String telefono;
-    private String eMail;
+    private String[] numTelefono;
+    private String[] eMail;
+    
+    private static int id=0;
+    private final int seq;
+   
+
+    public Contatto(String nome, String cognome) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.seq += id;
+    }
+    
+    public Contatto(String nome, String cognome, String[] numTelefono, String[] eMail) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.numTelefono = numTelefono;
+        this.eMail = eMail;
+        this.seq += id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public void setNumTelefono(String[] numTelefono) {
+        this.numTelefono = numTelefono;
+    }
+
+    public void seteMail(String[] eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String[] getNumTelefono() {
+        return numTelefono;
+    }
+
+    public String[] geteMail() {
+        return eMail;
+    }
+
+    public int getSeq() {
+        return seq;
+    }
+    
+    
+
+
+    
 }
