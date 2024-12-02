42d370d591e5a764d8d1892d33e52e1cdcf42f3a
rosap <rosap@172.19.166.74>
2024-12-02 15:16:56 +0100

    creazione dei package e delle classi

---
+++ src/main/java/progetto/contatto/Contatto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progetto.contatto;
+
+/**
+ *
+ * @author rosap
+ */
+public class Contatto {
+    private String nome;
+    private String cognome;
+    private String telefono;
+    private String eMail;
+}
