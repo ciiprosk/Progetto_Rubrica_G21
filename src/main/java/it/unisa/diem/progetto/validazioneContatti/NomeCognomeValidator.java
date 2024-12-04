09f551065bc43322af9c4264ee67748fb7f88c5d
rosap <rosap@172.19.166.74>
2024-12-04 20:34:05 +0100

    Rosa: creazione delle classi che implementano Validator

---
+++ src/main/java/it/unisa/diem/progetto/validazioneContatti/NomeCognomeValidator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.diem.progetto.validazioneContatti;
+
+/**
+ *
+ * @author rosap
+ */
+public class NomeCognomeValidator implements Validator{
+    public boolean validazione(String dato){
+        
+    }
+}
