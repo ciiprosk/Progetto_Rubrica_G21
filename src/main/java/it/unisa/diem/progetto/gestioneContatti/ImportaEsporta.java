1081a47aaaf944098ec9c661b94b5e8b751050c8
rosap <rosap@172.19.166.74>
2024-12-06 09:42:55 +0100

    commit di provacreazione della classe per importare ed esportare i dati

---
+++ src/main/java/it/unisa/diem/progetto/gestioneContatti/ImportaEsporta.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.diem.progetto.gestioneContatti;
+
+import it.unisa.diem.progetto.rubrica.Contatto;
+
+/**
+ *
+ * @author rosap
+ */
+public class ImportaEsporta {
+    public List<Contatto> importaContatto(){
+        
+    }
+    
+    public static void esporta(){
+        
+    }
+}
