526d7c78d625e93cc75bd467ef3a7b758c2b4f81
anton <anton@172.19.206.117>
2024-12-09 21:11:50 +0100

    Interfacce FXML e Aggiungi in it.unisa.diem.progetto.GUI

--- src/main/java/it/unisa/diem/progetto/GUI/FXMLController.java
+++ src/main/java/it/unisa/diem/progetto/GUI/FXMLController.java
@@ -5,9 +5,15 @@ package it.unisa.diem.progetto.GUI;
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 
+import it.unisa.diem.progetto.rubrica.Contatto;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,6 +21,21 @@ import javafx.fxml.Initializable;
  * @author rosap
  */
 public class FXMLController implements Initializable {
+
+    @FXML
+    private TextField searchBar;
+    @FXML
+    private Button plusPulsante;
+    @FXML
+    private TableView<Contatto> contattiTabella;
+    @FXML
+    private TableColumn<Contatto, String> nomeColonna;
+    @FXML
+    private TableColumn<Contatto, String> cognomeColonna;
+    @FXML
+    private TableView<Contatto> altContattiTabella;
+    @FXML
+    private TableColumn<Contatto, String> altNomeColonna;
 
     /**
      * Initializes the controller class.
