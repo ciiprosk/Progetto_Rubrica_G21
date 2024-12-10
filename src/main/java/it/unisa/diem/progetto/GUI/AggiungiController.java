7a9d7b9413bcc7ea9c2b43825894e165e91f2d7b
Omar <Utente@DESKTOP-RS8R3SR>
2024-12-10 18:15:28 +0100

    AggiungiController grandi miglioramenti dei controlli input errati

--- src/main/java/it/unisa/diem/progetto/GUI/AggiungiController.java
+++ src/main/java/it/unisa/diem/progetto/GUI/AggiungiController.java
@@ -4,6 +4,9 @@
  */
 package it.unisa.diem.progetto.GUI;
 
+import it.unisa.diem.progetto.validazioneContatti.EMailValidator;
+import it.unisa.diem.progetto.validazioneContatti.NumTelefonoValidator;
+import it.unisa.diem.progetto.validazioneContatti.Validator;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -56,10 +59,62 @@ public class AggiungiController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        salvaPulsante.setDisable(true);
+    
+    Validator telVal = new NumTelefonoValidator();
+    Validator eMailVal = new EMailValidator();
+        
+        primoTelefonoField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (telVal.verifica(newValue))
+                salvaPulsante.setDisable(false);
+            else salvaPulsante.setDisable(true);
+        });
+        
+        secondoTelefonoField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (telVal.verifica(newValue))
+                salvaPulsante.setDisable(false);
+            else salvaPulsante.setDisable(true);
+        });
+        
+        terzoTelefonoField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (telVal.verifica(newValue))
+                salvaPulsante.setDisable(false);
+            else salvaPulsante.setDisable(true);
+        });
+        
+        //CONTROLLO MAILS
+        
+        primaMailField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (eMailVal.verifica(newValue))
+                salvaPulsante.setDisable(false);
+            else salvaPulsante.setDisable(true);
+        });
+        
+        secondaMailField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (eMailVal.verifica(newValue))
+                salvaPulsante.setDisable(false);
+            else salvaPulsante.setDisable(true);
+        });
+        
+        terzaMailField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (eMailVal.verifica(newValue))
+                salvaPulsante.setDisable(false);
+            else salvaPulsante.setDisable(true);
+        });
+        
+        
     }    
+
+
+
 
     @FXML
     private void aggiungiContatto(ActionEvent event) {
+         
+    }
+
+    @FXML
+    private void switchToDefaultScene(ActionEvent event) {
     }
     
 }
