ebb5abe9404a31ef9a254d3a449a44c5005abe00
vincenzo <ix5lo@172.19.143.226>
2024-12-12 12:28:03 +0100

    aggiornato modificaController

--- src/main/java/it/unisa/diem/progetto/GUI/ModificaController.java
+++ src/main/java/it/unisa/diem/progetto/GUI/ModificaController.java
@@ -11,13 +11,18 @@ import it.unisa.diem.progetto.validazioneContatti.NomeCognomeValidator;
 import it.unisa.diem.progetto.validazioneContatti.NumTelefonoValidator;
 import it.unisa.diem.progetto.validazioneContatti.Validator;
 import java.net.URL;
+import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -26,24 +31,25 @@ import javafx.stage.Stage;
  * @author anton
  */
 public class ModificaController implements Initializable {
+
     @FXML
     private TextField nomeField;
-    
+
     @FXML
     private TextField cognomeField;
 
     @FXML
     private TextField primoTelefonoField;
-    
+
     @FXML
     private TextField secondoTelefonoField;
-    
+
     @FXML
     private TextField terzoTelefonoField;
 
     @FXML
     private TextField primaMailField;
-    
+
     @FXML
     private TextField secondaMailField;
 
@@ -52,19 +58,18 @@ public class ModificaController implements Initializable {
 
     @FXML
     private Button salvaPulsante;
-    
+
     @FXML
     private Button annullaPulsante;
-    
+
     private FXMLController fxmlController; //Riferimento al controller
-    
+
     private Rubrica rubrica; // Riferimento alla rubrica
-    
+
     private Contatto contatto; //Riferimento al contatto selezionato
-    
+
     private ObservableList<Contatto> observableList; //riferimento alla observableList
-    
-    
+
     /**
      * Initializes the controller class.
      */
@@ -73,97 +78,89 @@ public class ModificaController implements Initializable {
         // TODO
         // TODO
         salvaPulsante.setDisable(true);
-    
-    Validator telVal = new NumTelefonoValidator();
-    Validator eMailVal = new EMailValidator();
-    Validator nameVal = new NomeCognomeValidator();
-    Validator surnameVal = new NomeCognomeValidator();
-        
-    
+
+        Validator telVal = new NumTelefonoValidator();
+        Validator eMailVal = new EMailValidator();
+        Validator nameVal = new NomeCognomeValidator();
+        Validator surnameVal = new NomeCognomeValidator();
+
         //CONTROLLO NUMERI DI TELEFONO
-        
         primoTelefonoField.textProperty().addListener((observable, oldValue, newValue) -> {
-            controlloInput(telVal,eMailVal,nameVal,surnameVal);
+            controlloInput(telVal, eMailVal, nameVal, surnameVal);
         });
-        
+
         secondoTelefonoField.textProperty().addListener((observable, oldValue, newValue) -> {
-            controlloInput(telVal,eMailVal,nameVal,surnameVal);
+            controlloInput(telVal, eMailVal, nameVal, surnameVal);
         });
-        
+
         terzoTelefonoField.textProperty().addListener((observable, oldValue, newValue) -> {
-            controlloInput(telVal,eMailVal,nameVal,surnameVal);
+            controlloInput(telVal, eMailVal, nameVal, surnameVal);
         });
-        
-        
+
         //CONTROLLO MAILS
-        
         primaMailField.textProperty().addListener((observable, oldValue, newValue) -> {
-           controlloInput(telVal,eMailVal,nameVal,surnameVal);
+            controlloInput(telVal, eMailVal, nameVal, surnameVal);
         });
-        
+
         secondaMailField.textProperty().addListener((observable, oldValue, newValue) -> {
-            controlloInput(telVal,eMailVal,nameVal,surnameVal);
+            controlloInput(telVal, eMailVal, nameVal, surnameVal);
         });
-        
+
         terzaMailField.textProperty().addListener((observable, oldValue, newValue) -> {
-            controlloInput(telVal,eMailVal,nameVal,surnameVal);
+            controlloInput(telVal, eMailVal, nameVal, surnameVal);
         });
-        
 
         //CONTROLLO NOME E COGNOME
         nomeField.textProperty().addListener((observable, oldValue, newValue) -> {
-            controlloInput(telVal,eMailVal,nameVal,surnameVal);
-            
+            controlloInput(telVal, eMailVal, nameVal, surnameVal);
+
         });
-        
+
         cognomeField.textProperty().addListener((observable, oldValue, newValue) -> {
-            controlloInput(telVal,eMailVal,nameVal,surnameVal);
+            controlloInput(telVal, eMailVal, nameVal, surnameVal);
         });
-    } 
-    
+    }
+
     //Metodo per controllare l'input dei campi telefono
     public void controlloInput(Validator telVal, Validator eMailVal, Validator nameVal, Validator surnameVal) {
 
         boolean primoTel = telVal.verifica(primoTelefonoField.getText());
         boolean secondoTel = telVal.verifica(secondoTelefonoField.getText());
         boolean terzoTel = telVal.verifica(terzoTelefonoField.getText());
-        
+
         boolean primaMail = eMailVal.verifica(primaMailField.getText());
         boolean secondaMail = eMailVal.verifica(secondaMailField.getText());
         boolean terzaMail = eMailVal.verifica(terzaMailField.getText());
-        
+
         boolean nome = nameVal.verifica(nomeField.getText());
         boolean cognome = surnameVal.verifica(cognomeField.getText());
-        
-        boolean nomeInserito = nameVal.inserito (nomeField.getText() );
-        boolean cognomeInserito = surnameVal.inserito (cognomeField.getText() );
-        
-        
-        salvaPulsante.setDisable( ! (nome && cognome) ||  !(nomeInserito || cognomeInserito) || ! (primoTel && secondoTel && terzoTel) 
-        || ! (primaMail && secondaMail && terzaMail));
+
+        boolean nomeInserito = nameVal.inserito(nomeField.getText());
+        boolean cognomeInserito = surnameVal.inserito(cognomeField.getText());
+
+        salvaPulsante.setDisable(!(nome && cognome) || !(nomeInserito || cognomeInserito) || !(primoTel && secondoTel && terzoTel)
+                || !(primaMail && secondaMail && terzaMail));
     }
-    
-    
+
     // Metodo per impostare il riferimento alla rubrica
     public void setRubrica(Rubrica rubrica) {
         this.rubrica = rubrica;
     }
 
-
     // Metodo per impostare il controller principale
     public void setFXMLController(FXMLController fxmlController) {
         this.fxmlController = fxmlController;
     }
-    
+
     //Metodo per impostare il riferimento alla ObservableList
-    public void setObservableList(ObservableList<Contatto> observableList){
+    public void setObservableList(ObservableList<Contatto> observableList) {
         this.observableList = observableList;
     }
-    
+
     //Metodo per impostare il riferimento al contatto e preimpostare i textfield
-    public void setContatto(Contatto contatto){
+    public void setContatto(Contatto contatto) {
         this.contatto = contatto;
-        
+
         nomeField.setText(contatto.getNome());
         cognomeField.setText(contatto.getCognome());
         primoTelefonoField.setText(contatto.getNumTelefono1());
@@ -173,8 +170,8 @@ public class ModificaController implements Initializable {
         secondaMailField.setText(contatto.getEMail2());
         terzaMailField.setText(contatto.getEMail3());
     }
-    
-    public Contatto getContatto(){
+
+    public Contatto getContatto() {
         return this.contatto;
     }
 
@@ -187,27 +184,80 @@ public class ModificaController implements Initializable {
     @FXML
     private void modificaContatto(ActionEvent event) {
         //aggiungi
-        Contatto nuovoContatto = new Contatto(cognomeField.getText(), nomeField.getText(), primoTelefonoField.getText(), secondoTelefonoField.getText(), terzoTelefonoField.getText(), primaMailField.getText(), secondaMailField.getText(), terzaMailField.getText());
-        rubrica.aggiungiContatto(nuovoContatto);
-        fxmlController.aggiornaTabella();
-        
-        //elimina vecchio
-        Contatto contattoSelezionato = this.getContatto();
-        if (contattoSelezionato == null) {
-            System.out.println("Nessun contatto selezionato per l'eliminazione.");
-            return;
+        List<Contatto> contatti = rubrica.esisteDuplicato(cognomeField.getText() + " " + nomeField.getText());
+
+        if (contatti.isEmpty()) {
+
+            Contatto nuovoContatto = new Contatto(cognomeField.getText(), nomeField.getText(), primoTelefonoField.getText(), secondoTelefonoField.getText(), terzoTelefonoField.getText(), primaMailField.getText(), secondaMailField.getText(), terzaMailField.getText());
+            rubrica.aggiungiContatto(nuovoContatto);
+            fxmlController.aggiornaTabella();
+
+            //elimina vecchio
+            Contatto contattoSelezionato = this.getContatto();
+            if (contattoSelezionato == null) {
+                System.out.println("Nessun contatto selezionato per l'eliminazione.");
+                return;
+            }
+
+            int contactId = contattoSelezionato.getId(); // Ottieni l'ID dal contatto
+            System.out.println("Eliminazione contatto con ID: " + contactId);
+
+            rubrica.eliminaContatto(contattoSelezionato); // Elimina dal database usando l'ID
+            observableList.remove(contattoSelezionato);
+            fxmlController.aggiornaTabella();
+
+            Stage stage = (Stage) salvaPulsante.getScene().getWindow();
+            stage.close();
+
+        } else {
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Creazione contatto omonimo?");
+            alert.setHeaderText("Esiste già un contatto con quella combinazione cognome-nome.");
+            alert.setContentText("Vuoi modificare quello già esistente?");
+
+            ButtonType buttonTypeYes = new ButtonType("Sì");
+            ButtonType buttonTypeCreaNuovo = new ButtonType("Crea Nuovo");
+            alert.initModality(Modality.APPLICATION_MODAL);
+
+            alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeCreaNuovo, ButtonType.CANCEL);
+
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.isPresent()) {
+                if (result.get() == buttonTypeYes) {
+                    // fai visualizzare la lista
+                    switchToDefaultScene(event);
+                    fxmlController.setSearchBar(cognomeField.getText() + " " + nomeField.getText());
+
+                    Stage stage = (Stage) salvaPulsante.getScene().getWindow();
+                    stage.close();
+                } else if (result.get() == buttonTypeCreaNuovo) {
+                    Contatto nuovoContatto = new Contatto(cognomeField.getText(), nomeField.getText(), primoTelefonoField.getText(), secondoTelefonoField.getText(), terzoTelefonoField.getText(), primaMailField.getText(), secondaMailField.getText(), terzaMailField.getText());
+                    rubrica.aggiungiContatto(nuovoContatto);
+                    fxmlController.aggiornaTabella();
+
+                    //elimina vecchio
+                    Contatto contattoSelezionato = this.getContatto();
+                    if (contattoSelezionato == null) {
+                        System.out.println("Nessun contatto selezionato per l'eliminazione.");
+                        return;
+                    }
+
+                    int contactId = contattoSelezionato.getId(); // Ottieni l'ID dal contatto
+                    System.out.println("Eliminazione contatto con ID: " + contactId);
+
+                    rubrica.eliminaContatto(contattoSelezionato); // Elimina dal database usando l'ID
+                    observableList.remove(contattoSelezionato);
+                    fxmlController.aggiornaTabella();
+
+                    Stage stage = (Stage) salvaPulsante.getScene().getWindow();
+                    stage.close();
+
+                }
+            }
         }
 
-        int contactId = contattoSelezionato.getId(); // Ottieni l'ID dal contatto
-        System.out.println("Eliminazione contatto con ID: " + contactId);
-
-        rubrica.eliminaContatto(contattoSelezionato); // Elimina dal database usando l'ID
-        observableList.remove(contattoSelezionato);
-        fxmlController.aggiornaTabella();
-        
-        Stage stage = (Stage) salvaPulsante.getScene().getWindow(); 
-        stage.close();
-        
     }
-    
+
 }
